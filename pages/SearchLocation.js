50570adebda7e12697a238d78acdbeaa45a72c89
Alberto Maria Carotenuto <carotenuto.a.m@gmail.com>
2020-12-04 18:17:11 +0100

    added backend structure and database connection

--- pages/SearchLocation.js
+++ pages/SearchLocation.js
@@ -4,77 +4,53 @@ import React, { useState } from 'react'
 import Link from 'next/link'
 
 import AutocompletePlace from './AutocompletePlace'
+import InputLocation from "./components/InputLocation";
 
-class Search extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      place: null,
-    }
-    this.handleSelect = this.handleSelect.bind(this)
-  }
-  handleSelect(place) {
-    this.setState({ place })
-  }
-  render() {
-    return (
-      <div>
-        <AutocompletePlace onSelect={this.handleSelect} />
-      </div>
-    );
-  }
-}
-
-
-function LocationForm() {
-
-  const [value, setValue] = useState('')
-
-  function handleChange(event) {
-    setValue(event.target.value);
-  }
-
-  function handleSubmit(event) {
-    event.preventDefault()
-    fetch("http://localhost:3001/", {
-      method: 'POST',
-      body: JSON.stringify({ location: [{ location: value }] }),
-      headers: { 'Content-Type': 'application/json' },
-    })
-      .then(res => res.json())
-      .then(json => setValue(json.value))
-  }
-
-  return (
-    <form className="OnSubmitStyle" onSubmit={handleSubmit}>
-      <label>
-        <div>Choose your destination:</div>
-        <Search type="text" value={value} onChange={handleChange}/>
-      </label>
-      <Link href="./CreateNewTrip"><input type="submit" value="Submit" /></Link>
-    </form>
-  );
-
-}
+// const LocationForm = () => {
+//   const onSubmitForm = async e => {
+//     e.preventDefault();
+//     try {
+//       const body = { location };
+//       const response = await fetch("http://localhost:3002/location", {
+//         method: "POST",
+//         headers: { "Content-Type": "application/json" },
+//         body: JSON.stringify(body)
+//       });
+//       console.log(response)
+//     } catch (err) {
+//       console.error(err.message);
+//     }
+//   }
+  // function handleSelect(place) {
+  //   setLocation({ place })
+  // }
+//   const [location, setLocation] = useState("")
+//   return (
+//     <form className="OnSubmitStyle" onSubmit={onSubmitForm}>
+//       <label>
+//         <input type="text" value={location} onChange={e => setLocation(e.target.value)}></input>
+//         <div>Choose your destination:</div>
+//       </label>
+//       <Link href="./CreateNewTrip"><button>Ammazzati</button></Link>
+//     </form>
+//   );
+// }
 
 function SearchLocation() {
-
-
   return (
     <div>
 
       <TopNavbar />
 
       <img className="LocationLogo" src="trip2day_logo.png" width="30%" />
-
       <div className='BackgroundLocation'>
         <h1 className="TextCenter" >Prepare for a new adventure!</h1>
         <div className="LocationStyle">
-          <LocationForm/>
+          <InputLocation />
         </div>
       </div>
       <br />
-      
+
     </div>
   );
 }
